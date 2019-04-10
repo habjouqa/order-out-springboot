74a2b8ac3850e7f4cf148a7c319700cddeec85bb
Anas Hijazi <anas_hijazy@yahoo.com>
2019-04-10 22:16:25 +0300

    Add Send Email

--- src/main/java/com/orderout/orderout/services/impl/UserServiceImpl.java
+++ src/main/java/com/orderout/orderout/services/impl/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package com.orderout.orderout.services.impl;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +19,13 @@ import com.orderout.orderout.domain.User;
 import com.orderout.orderout.domain.UserDto;
 import com.orderout.orderout.services.MailService;
 import com.orderout.orderout.services.UserService;
-import com.orderout.orderout.services.constant.Template;
+import com.sendgrid.Content;
+import com.sendgrid.Email;
+import com.sendgrid.Mail;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
 
 @Service(value = "userService")
 public class UserServiceImpl implements UserDetailsService, UserService {
@@ -106,7 +112,25 @@ public class UserServiceImpl implements UserDetailsService, UserService {
 		newUser.setPhoneNumber(user.getPhoneNumber());
 		try {
 			
-		mail.sendEmail(newUser,Template.SUBJECT_ACTIVATION_MESSAGE,Template.ACTIVATION_MESSAGE);
+			Email from = new Email("test@example.com");
+		    String subject = "Hello World from the SendGrid Java Library!";
+		    Email to = new Email("test@example.com");
+		    Content content = new Content("text/plain", "Hello, Email!");
+		    Mail mail = new Mail(from, subject, to, content);
+
+		    SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+		    Request request = new Request();
+		    try {
+		      request.setMethod(Method.POST);
+		      request.setEndpoint("mail/send");
+		      request.setBody(mail.build());
+		      Response response = sg.api(request);
+		      System.out.println(response.getStatusCode());
+		      System.out.println(response.getBody());
+		      System.out.println(response.getHeaders());
+		    } catch (IOException ex) {
+		      throw ex;
+		    }
 		
 		}catch (Exception e) {
 			System.err.println(" ########### SEND EMAIL FAILD ###########3");
