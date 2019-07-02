e0fcdd0a690ae11b5e75bd6118fac895666421be
habjouqa <mohhabjouqa@gmail.com>
2019-07-02 16:46:00 +0300

    fix cors

--- src/main/java/com/orderout/orderout/controller/GroupOrdersController.java
+++ src/main/java/com/orderout/orderout/controller/GroupOrdersController.java
@@ -1,12 +1,14 @@
 package com.orderout.orderout.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.orderout.orderout.domain.GroupOrder;
 import com.orderout.orderout.service.GroupOrderBusiness;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class GroupOrdersController {
 
