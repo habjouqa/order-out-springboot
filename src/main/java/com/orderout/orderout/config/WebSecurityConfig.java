05a99151fc532b5b5585bd40ae8233d2bde38070
habjouqa <mohhabjouqa@gmail.com>
2019-07-02 13:33:12 +0300

    TEMP - REMOVE SECURITY FROM GROUP ORDERS

--- src/main/java/com/orderout/orderout/config/WebSecurityConfig.java
+++ src/main/java/com/orderout/orderout/config/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().authorizeRequests()
 				.antMatchers("/token/*", "/signup", "/api/products", "/isExistsEmail", "/activate",
-						"/sendVerification","/reset","/order_deadline").permitAll()
+						"/sendVerification","/reset","/order_deadline", "/group-orders").permitAll()
 				.anyRequest().authenticated().and().exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
 				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
